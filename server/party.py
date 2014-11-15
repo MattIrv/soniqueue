7fe62d3bead60ca5b07c8876cb6348fb0049518d
ChrisEarman <cje4sw@virginia.edu>
2014-11-15 14:42:44 -0500

    added song object and updated UserQueue object

--- server/party.py
+++ server/party.py
@@ -6,9 +6,8 @@ class Party(object):
 		self.host = host
 		self.location = location
 		self.userDict = {}
-		self.PartyQueue = queues.PartyQueue()
+		self.queue = queues.PartyQueue()
 	def __str__(self):
 		return "Party(%s, %s, %s)" % (str(name), str(host), str(location))
 	def __repr__(self):
 		return "Party(%s, %s, %s)" % (str(name), str(host), str(location))
-
--- server/queues.py
+++ server/queues.py
@@ -1,9 +1,22 @@
-
+import song
 
 class UserQueue(object):
 	def __init__(self):
-		pass
-
+		self.queue = []
+	def addSong(self, URI):
+		self.queue.append(Song(URI,cur_sID))
+	def removeSong(self, sID):
+		self.queue.remove(song_map[sID]) #does not remove the sID from the song_map dictionary
+	def clearQueue(self):
+		self.queue = []
+	def top(self): #only shows what top song is, does not remove it
+		if self.queue:
+			return self.queue[0]
+		else:
+			return None
+	def pop(self): #only removes top song, does not show what top song is
+		if self.queue:
+			self.removeSong(self.top().sID)
 class PartyQueue(object):
 	def __init__(self):
 		pass
