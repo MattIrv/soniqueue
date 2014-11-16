3a4fa25ede8b030d8113aac6bc1bfb58e7481216
ChrisEarman <cje4sw@virginia.edu>
2014-11-16 09:08:38 -0500

    added return json to the get_user_position server functionality

--- server/util.py
+++ server/util.py
@@ -153,7 +153,7 @@ def get_position(p_id, u_id):
     # try:
         cur_user = user_map[u_id]
         cur_party = party_map[p_id]
-        return cur_party.get_user_position(user)
+        return json.dumps({'user_pos': str(cur_party.get_user_position(user))})
     # except Exception as e:
         return json.dumps({'err': str(e)})
 
